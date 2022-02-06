c150714a8cf957195aa277ab76e50fcfb756a973
Kayle Barreto <kayle.barrt@gmail.com>
2022-02-06 01:12:10 -0300

    axios

---
+++ src/services/index.tsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export async function getAllPlanets() {
+  const response = await axios.get(
+    "https://swapi-trybe.herokuapp.com/api/planets"
+  );
+  return response.data.results;
+}
