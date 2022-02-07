03c091f9f2b83ef9faf220c9c952521ba7b17535
Kayle Barreto <kayle.barrt@gmail.com>
2022-02-07 02:17:22 -0300

    updated home page

--- src/components/screens/HomeScreen/index.tsx
+++ src/components/screens/HomeScreen/index.tsx
@@ -1,30 +1,18 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import PlanetsContext from "../../../context/PlanetsContext";
+import { TitleNavBar } from "../../MainLayout/TitleNavBar";
 import { Table } from "../../Table";
 import { LoadingSpinner } from "../../LoadingSpinner";
-import { InputFilterByName } from "../../Filters/FilterByName";
+import { PlanetsFilters } from "../../Filters";
 
 export const HomeScreen = () => {
-  const { loading, filters, setFilters } = useContext(PlanetsContext);
+  const { loading } = useContext(PlanetsContext);
 
   return (
     <div>
-      <div className="sticky top-0 flex items-center justify-center px-4 py-3 text-sm font-semibold text-white bg-slate-700/70 backdrop-blur-sm ring-1 ring-black/10">
-        <h1 className="text-2xl font-bold text-gray-200 md:text-3xl">
-          Planets Star Wars
-        </h1>
-      </div>
-
+      <TitleNavBar title="Planets Star Wars" />
       <div className="flex flex-col space-y-5">
-        <div className="flex flex-row">
-          <InputFilterByName
-            label="Search by name"
-            onChange={(e) =>
-              setFilters({ ...filters, filterByName: { name: e.target.value } })
-            }
-            value={filters.filterByName.name}
-          />
-        </div>
+        <PlanetsFilters />
         {loading ? (
           <LoadingSpinner description="This may take a few seconds, please do not close this page." />
         ) : (
